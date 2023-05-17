f7dae9d6fe8a03c5033db1777d51dd2ed229e760
Naveen Verma <naveen35.nith@gmail.com>
2023-05-17 16:57:43 +0530

    changed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.where(user: nil).delete_all
+User.where(email: "naveen35.nith@gmail.com").update(admin: true)
